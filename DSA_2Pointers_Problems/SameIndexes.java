06e9029512820ff32f6d4a09acb00f4242a5cefd
Hitesh Vinod Shimpi <43511832+hiteshshimpi@users.noreply.github.com>
2025-02-21 18:04:49 +0530

    Sme indexes matching value

---
+++ DSA_2Pointers_Problems/SameIndexes.java
@@ -0,0 +1,23 @@
+package DSA_2Pointers_Problems;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SameIndexes {
+
+
+    public static void main(String[] args) {
+        int [] a ={1,4,5,7};
+        int b[] ={6,4,3,7};
+        List<Integer> newList = new ArrayList<>();
+
+        for(int i=0;i<a.length;i++)
+        {
+            if(a[i]==b[i])
+            {
+                newList.add(a[i]);
+            }
+        }
+        System.out.println(newList.toString());
+    }
+}
