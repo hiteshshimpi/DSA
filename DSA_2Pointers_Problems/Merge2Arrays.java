8fe5ef8aaa8f502d6830fcda1e9cb14ca02282ce
Hitesh Vinod Shimpi <43511832+hiteshshimpi@users.noreply.github.com>
2025-03-09 18:38:45 +0530

    Merging 2 Arrays

--- DSA_2Pointers_Problems/Merge2Arrays.java
+++ DSA_2Pointers_Problems/Merge2Arrays.java
@@ -1,2 +1,26 @@
-package DSA_2Pointers_Problems;public class Merge2Arrays {
+package DSA_2Pointers_Problems;
+
+import java.util.Arrays;
+
+public class Merge2Arrays {
+    public static void main(String[] args) {
+
+        int [] arr1 ={1,2,3};
+        int [] arr2={4,5,6};
+        int [] arr3 = new int[arr1.length+arr2.length];
+        System.out.println(Arrays.toString(arr3));
+            int len =arr3.length;
+            for(int i=0;i<arr1.length;i++)
+            {
+                arr3[i]=arr1[i];
+            }
+        System.out.println(Arrays.toString(arr3));
+            int n =arr1.length;
+            for(int i =0;i<arr2.length;i++)
+            {
+                arr3[n]=arr2[i];
+                n++;
+            }
+        System.out.println(Arrays.toString(arr3));
+    }
 }
